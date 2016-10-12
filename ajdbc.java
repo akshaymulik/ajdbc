b3d3aaae7f35fd0c50b7b9522a9e75723aa39d48
Akshay Sanjay Mulik <akshaymulik399@gmail.com>
2016-10-12 19:42:13 +0530

    Update ajdbc.java

--- ajdbc.java
+++ ajdbc.java
@@ -6,8 +6,8 @@ public class ajdbc
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
    static final String DB_URL = "jdbc:mysql://localhost/ajdbc";
    //  Database credentials
-   static final String USER = "root";
-   static final String PASS = "toor";
+   static final String USER = "system";
+   static final String PASS = "tiger";
 
    public static boolean ajs(String uid)
    {
